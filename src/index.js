bbd3646b1d9b396eb2089a827ff891f9cf7c271c
Huu Quyet Nguyen <kyetnguyennbu@gmail.com>
2023-02-24 08:02:45 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -5,11 +5,29 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+class Item extends React.Component{
+  render (){
+    return <div className="item">
+    <b>Name:</b> {this.props.name} <br />
+    <b>Price:</b> ${this.props.price}
+    </div>;
+  }
+}
+
+function Fruit(){
+  return <div>
+    <Item name="Banana" price="4"/>
+    <Item name="Orange" price="5"/>
+    <Item name="Strawberry" price="10"/>
+  </div>
+}
+
+const el = <Fruit/>
+
+root.render(el)
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
